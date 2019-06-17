a00c62e9102ab171c4b9224a4aaf622eb4c1aebd
丹下輝巳 <teru.0701.tt@gmail.com>
2019-06-17 16:02:17 +0900

    バリア追加

--- OngekiShooting/Assets/Scripts/Player/PlayerMove.cs
+++ OngekiShooting/Assets/Scripts/Player/PlayerMove.cs
@@ -7,14 +7,35 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody rigidbody;
     public float speed = 5;
+    public GameObject barrier;
+    public bool barrierFlag;//バリア状態かどうか
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        barrierFlag = true;
+        barrier = transform.Find("Barrier").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         rigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0, 0);
+        Barrier();
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            if (barrierFlag) barrierFlag = false;
+            else barrierFlag = true;
+        }
+    }
+
+    void Barrier()
+    {
+        switch(barrierFlag)
+        {
+            case true:
+                barrier.SetActive(true);break;
+            case false:
+                barrier.SetActive(false);break;
+        }
     }
 }
