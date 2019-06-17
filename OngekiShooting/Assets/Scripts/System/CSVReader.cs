4203fea3d619248e4cf4c708d8245a1e0b502620
内田幸司 <koji0126gogo@yahoo.co.jp>
2019-06-17 11:09:45 +0900

    CSV読み込み(途中)
    ・CSV読み込みクラスを作成

---
+++ OngekiShooting/Assets/Scripts/System/CSVReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static  class CSVReader
+{
+    public static List<EnemySpawnData> Read(string fileName)
+    {
+        List<EnemySpawnData> data = new List<EnemySpawnData>();
+
+        string filePath = Application.streamingAssetsPath + fileName + ".csv";
+        using (StreamReader stream = new StreamReader(File.Open(filePath, FileMode.Open)))
+        {
+
+        }
+        return data;
+    }
+}
---
+++ OngekiShooting/Assets/Scripts/System/EnemySpawnData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnData
+{
+    Vector3 position;
+    EnemyType enemyType;
+    MoveType moveType;
+
+    public EnemySpawnData(Vector3 position, EnemyType enemyType, MoveType moveType)
+    {
+        this.position = position;
+        this.enemyType = enemyType;
+        this.moveType = moveType;
+    }
+}
+
+public enum EnemyType
+{
+
+}
+
+public enum MoveType
+{
+
+}
