1a5306907314da9d13a3ef3b909e32c9bba49952
丹下輝巳 <teru.0701.tt@gmail.com>
2019-06-17 14:09:29 +0900

    追いかけてくる敵追加

---
+++ OngekiShooting/Assets/Scripts/AI/TrackingAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingAI : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public GameObject player;
+    private Rigidbody playerRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        playerRigid = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (playerRigid == null) return;
+        //rigidbody.position += new Vector3(playerRigid.position.x - rigidbody.position.x, 0, playerRigid.position.z - rigidbody.position.z) * Time.deltaTime;
+        rigidbody.position += new Vector3(playerRigid.position.x - rigidbody.position.x, 0, -10) * Time.deltaTime;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
