54d01667e3df3ddb012e45e6717001e480ced10e
内田幸司 <koji0126gogo@yahoo.co.jp>
2019-06-17 12:05:46 +0900

    CSV読み込みクラスを作成
    ・CSVで作成した敵の生成データを読み込めるように

--- OngekiShooting/Assets/Scripts/System/CSVReader.cs
+++ OngekiShooting/Assets/Scripts/System/CSVReader.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using System.IO;
 
-public static  class CSVReader
+public static class CSVReader
 {
-    public static List<EnemySpawnData> Read(string fileName)
+    public static List<EnemySpawnData> ReadEnemySpawnData(string fileName, float y)
     {
         List<EnemySpawnData> data = new List<EnemySpawnData>();
 
         string filePath = Application.streamingAssetsPath + fileName + ".csv";
-        using (StreamReader stream = new StreamReader(File.Open(filePath, FileMode.Open)))
+        using (StreamReader sr = new StreamReader(File.Open(filePath, FileMode.Open)))
         {
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
 
+                var values = line.Split(',');
+
+                float x = float.Parse(values[0]);
+                float z = float.Parse(values[1]);
+                EnemyType enemyType = (EnemyType)Enum.Parse(typeof(EnemyType), values[2]);
+                MoveType moveType = (MoveType)Enum.Parse(typeof(MoveType), values[3]);
+
+                data.Add(new EnemySpawnData(new Vector3(x, y, z), enemyType, moveType));
+            }
         }
         return data;
     }
