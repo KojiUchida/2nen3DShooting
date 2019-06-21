4d7ddbef59cfc9902c8e2717f2e4d201a7fbed99
内田幸司 <koji0126gogo@yahoo.co.jp>
2019-06-21 10:21:15 +0900

    死亡時弾を撃つ敵を追加
    ・死亡時弾を撃つ敵を追加
    ・フォルダの変更
    ・弾反射の敵を追加(未完了)

---
+++ OngekiShooting/Assets/Scripts/Enemy/AI/ReflectAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedflectAI : AI
+{
+    [SerializeField, Header("弾のプレファブ")]
+    protected GameObject enemyBullet;
+    [SerializeField, Header("弾の間隔")]
+    private float shotInterval = 0.5f;
+
+    float count;
+
+    public override void Init()
+    {
+        base.Init();
+        count = shotInterval;
+    }
+
+    public override void Attack()
+    {
+        Shot();
+    }
+
+    private void Shot()
+    {
+        count -= Time.deltaTime;
+        if (count > 0) return;
+        count = shotInterval;
+        Instantiate(enemyBullet, transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ReflectBullet(other);
+    }
+
+    private void ReflectBullet(Collider other)
+    {
+        if (other.tag != "PlayerBullet") return;
+        var bullet = other.GetComponent<PlayerBullet>();
+        //other.gameObject
+    }
+}
