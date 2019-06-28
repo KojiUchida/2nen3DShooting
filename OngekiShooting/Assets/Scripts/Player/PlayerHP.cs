da95c384649a4688497934fc1bf8ae2168b8582a
丹下輝巳 <teru.0701.tt@gmail.com>
2019-06-28 16:28:07 +0900

    player
    点滅処理のメソッド追加
    処理途中（内田よろのところ）
    ↑53行目

--- OngekiShooting/Assets/Scripts/Player/PlayerHP.cs
+++ OngekiShooting/Assets/Scripts/Player/PlayerHP.cs
@@ -8,9 +8,13 @@ public class PlayerHP : MonoBehaviour
     public int hp;//ヒットポイント
     public float invincibleTime = 3;//無敵時間
     private float countTime;//時間カウント
+
+    private bool isDamage;
+
     void Start()
     {
         countTime = 0;
+        isDamage = false;
     }
 
     // Update is called once per frame
@@ -32,14 +36,27 @@ public class PlayerHP : MonoBehaviour
     {
         bool damageTag = other.tag == "EnemyBullet" || other.tag == "EnemyReflectBullet";
         if (!damageTag) return;
-        if (invincibleTime > countTime) return;
+        if (isDamage) return;
         var bullet = other.GetComponent<Bullet>();
-        hp -= bullet.GetDamage();
+        Damage(bullet.GetDamage());
         countTime = 0;
     }
 
     public void Damage(int damage)
     {
         hp -= damage;
+        StartCoroutine(Blink());
+    }
+
+    IEnumerator Blink()
+    {
+        isDamage = true;
+        
+        //点滅の計算修正せよ
+        float level = Mathf.Abs(Mathf.Sin(Time.time * 10f));
+        gameObject.GetComponent<MeshRenderer>().material.color = new Color(1f, 1f, 1f, 0.5f);//ここを修正せよ（内田！！）
+        yield return new WaitForSeconds(invincibleTime);
+        isDamage = false;
     }
 }
+
