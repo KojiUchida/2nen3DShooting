92193d6edb9faf495830c5f56f62337d1bf21e5d
内田幸司 <koji0126gogo@yahoo.co.jp>
2019-06-30 02:01:19 +0900

    点滅処理
    ・プレイヤーがダメージを受けた際の点滅処理を作成
    ・プレイヤーをプレファブ化
    ・プレイヤーのマテリアルを一部変更

--- OngekiShooting/Assets/Scripts/Player/PlayerHP.cs
+++ OngekiShooting/Assets/Scripts/Player/PlayerHP.cs
@@ -7,21 +7,22 @@ public class PlayerHP : MonoBehaviour
     // Start is called before the first frame update
     public int hp;//ヒットポイント
     public float invincibleTime = 3;//無敵時間
-    private float countTime;//時間カウント
 
     private bool isDamage;
+    Material mat;
 
     void Start()
     {
-        countTime = 0;
         isDamage = false;
+        mat = gameObject.GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isDamage);
         Death();
-        countTime += Time.deltaTime;
+        Blink();
     }
 
     void Death()
@@ -39,24 +40,28 @@ public class PlayerHP : MonoBehaviour
         if (isDamage) return;
         var bullet = other.GetComponent<Bullet>();
         Damage(bullet.GetDamage());
-        countTime = 0;
     }
 
     public void Damage(int damage)
     {
+        if (isDamage) return;
         hp -= damage;
-        StartCoroutine(Blink());
+        StartCoroutine(DamageCoroutine());
     }
 
-    IEnumerator Blink()
+    IEnumerator DamageCoroutine()
     {
         isDamage = true;
-        
-        //点滅の計算修正せよ
-        float level = Mathf.Abs(Mathf.Sin(Time.time * 10f));
-        gameObject.GetComponent<MeshRenderer>().material.color = new Color(1f, 1f, 1f, 0.5f);//ここを修正せよ（内田！！）
         yield return new WaitForSeconds(invincibleTime);
         isDamage = false;
+        mat.color = new Color(1f, 1f, 1f, 1);
+    }
+
+    void Blink()
+    {
+        if (!isDamage) return;
+        float level = Mathf.Abs(Mathf.Sin(Time.time * 10f));
+        mat.color = new Color(1f, 1f, 1f, level);
     }
 }
 
